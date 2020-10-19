201c5bd74d93ad61a01f6edc47b439d39541c765
Bruno SANCHEZ <bruno.sanchez@duke.edu>
2020-10-19 16:43:29 -0400

    fixed setup.py and finished first test. Fixes #23

--- tests/test_input.py
+++ tests/test_input.py
@@ -11,9 +11,6 @@ import pytest
 
 import numpy as np
 
-
-import sys
-sys.path.insert(0, '.')
 from galaxychop import utils
 
 # =============================================================================
@@ -26,15 +23,35 @@ random = np.random.RandomState(seed=31)
 # =============================================================================
 # Defining utility functions for mocking data
 # =============================================================================
-def solid_disc(N_part=100, rmax=30, rmin=5, omega=10):
+def solid_disk(N_part=100, rmax=30, rmin=5, omega=10):
     """
-    Creates a set of particles that belong to a rigid body rotating disc,
-    sampling particles from a flat annulus, with maximum radius and minimum 
+    Creates a set of particles that belong to a rigid body rotating disk,
+    sampling particles from a flat annulus, with maximum radius and minimum
     radius `(rmax, rmin)`.
-    
-    The angular velocity `omega` is used to set an angular rotation around 
-    `z` axis. The function delivers mass vector (set to identical unity mass),
 
+    The angular velocity `omega` is used to set an angular rotation around
+    `z` axis. The function delivers mass vector (set to identical unity mass),
+    the positions and velocities as `(N_part, 3)` arrays.
+
+    Parameters
+    ----------
+    N_part : `int`
+        The total number of particles to obtain
+    rmax : `float`
+        The maximum radius of the disk
+    rmin : `float`
+        The minimum radius of the disk
+    omega : `float`
+        Angular velocity of the
+
+    Returns
+    -------
+    mass : `np.ndarray`, shape = N_part, 1
+        Masses per particle, identically 1 for all.
+    pos : `np.ndarray`, shape = N_part, 3
+        Positions of particles
+    vel : `np.ndarray`, shape = N_part, 3
+        Velocities of particles
     """
     r = (rmax - rmin) * np.random.random_sample(size=N_part) + rmin
     phi0 = 2 * np.pi * np.random.random_sample(size=N_part)
@@ -52,7 +69,7 @@ def solid_disc(N_part=100, rmax=30, rmin=5, omega=10):
     vel = np.array([xdot, ydot, zdot]).T
     return mass, pos, vel
 
-# transformation matrix
+
 def rot_matrix_xaxis(theta=0):
     """
     Rotation matrix of a transformation around X axis.
@@ -61,19 +78,22 @@ def rot_matrix_xaxis(theta=0):
     ----------
     theta : `float`
         Rotation angle in radians
+
     Returns
     -------
     A : `np.ndarray`
         Rotation matrix, with shape (3, 3)
 
     """
-    A = np.array([
-        [1, 0, 0],
-        [0, np.cos(theta),  -1 * np.sin(theta)],
-        [0, np.sin(theta), np.cos(theta)],
+    A = np.array(
+        [
+            [1, 0, 0],
+            [0, np.cos(theta),  -1 * np.sin(theta)],
+            [0, np.sin(theta), np.cos(theta)],
         ]
     )
     return A
+
 
 def rot_matrix_yaxis(theta=0):
     """
@@ -89,13 +109,15 @@ def rot_matrix_yaxis(theta=0):
         Rotation matrix, with shape (3, 3)
 
     """
-    A = np.array([
-        [np.cos(theta), 0, -1 * np.sin(theta)],
-        [0, 1,  0],
-        [np.sin(theta), 0, np.cos(theta)],
+    A = np.array(
+        [
+            [np.cos(theta), 0, -1 * np.sin(theta)],
+            [0, 1,  0],
+            [np.sin(theta), 0, np.cos(theta)],
         ]
     )
     return A
+
 
 def rot_matrix_zaxis(theta=0):
     """
@@ -111,21 +133,40 @@ def rot_matrix_zaxis(theta=0):
         Rotation matrix, with shape (3, 3)
 
     """
-    A = np.array([
-        
-        [np.cos(theta),  -1 * np.sin(theta), 0],
-        [np.sin(theta), np.cos(theta), 0],
-        [0, 0, 1],
+    A = np.array(
+        [
+            [np.cos(theta),  -1 * np.sin(theta), 0],
+            [np.sin(theta), np.cos(theta), 0],
+            [0, 0, 1],
         ]
     )
     return A
 
-# rotation function
-def rotate(pos, vel, matrix):
 
+def rotate(pos, vel, matrix):
+    """
+    Apply the rotation `matrix` to a set of particles positions `pos` and
+    velocities `vel`
+
+    Parameters
+    ----------
+    pos : `np.ndarray`, shape = N_part, 3
+        Positions of particles
+    vel : `np.ndarray`, shape = N_part, 3
+        Velocities of particles
+    matrix : `np.ndarray`
+        Rotation matrix, with shape (3, 3)
+
+    Returns
+    -------
+    pos_rot : `np.ndarray`, shape = N_part, 3
+        Rotated, positions of particles
+    vel_rot : `np.ndarray`, shape = N_part, 3
+        Rotated, velocities of particles
+    """
     pos_rot = pos @ matrix
     vel_rot = vel @ matrix
-    
+
     return pos_rot, vel_rot
 
 
@@ -135,14 +176,15 @@ def rotate(pos, vel, matrix):
 
 @pytest.fixture
 def disc_zero_angle():
-    m, pos, vel = solid_disc(N_part=1000)
+    m, pos, vel = solid_disk(N_part=1000)
 
     return m, pos, vel
 
+
 @pytest.fixture
 def disc_xrotation():
-    m, pos, vel = solid_disc(N_part=1000)
-    a = rot_matrix_xaxis(theta=0.5* np.pi * np.random.random())
+    m, pos, vel = solid_disk(N_part=1000)
+    a = rot_matrix_xaxis(theta=0.5 * np.pi * np.random.random())
 
     return m, pos @ a, vel @ a, a
 
@@ -151,7 +193,7 @@ def disc_xrotation():
 # TESTS
 # =============================================================================
 
-# rotate it 
+
 def test_getrotmat0(disc_zero_angle):
     gxchA = utils._get_rot_matrix(*disc_zero_angle)
 
@@ -160,6 +202,7 @@ def test_getrotmat0(disc_zero_angle):
     np.testing.assert_allclose(0., gxchA[2, 0], rtol=1e-4, atol=1e-3)
     np.testing.assert_allclose(0., gxchA[0, 2], rtol=1e-4, atol=1e-3)
     np.testing.assert_allclose(0., gxchA[1, 2], rtol=1e-4, atol=1e-3)
+
 
 def test_invert_xaxis(disc_xrotation):
     m, pos, vel, a = disc_xrotation
