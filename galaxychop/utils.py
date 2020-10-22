a056ab95285cf72b2192c4d45c084976cdce01f7
Ornela Marioni <ornela@orne.iate.oac.uncor.edu>
2020-10-22 19:23:32 -0300

    Revert "style fixes" -  To avoid merge conflict in galaxychop.py
    This reverts commit 7a5433edf9e44fed26204b19c3c593e1e10396b9.

--- galaxychop/utils.py
+++ galaxychop/utils.py
@@ -4,9 +4,9 @@ import numpy as np
 def _get_rot_matrix(m, pos, vel, r_corte=None):
     """
     Calculates the rotation matrix that aligns the TOTAL
-    agular momentum of the particles with the z-axis. The
-    positions, velocities and masses of the particles are
-    used. Optionally, only particles within a cutting
+    agular momentum of the particles with the z-axis. The 
+    positions, velocities and masses of the particles are 
+    used. Optionally, only particles within a cutting 
     radius `(r_corte)` can be used.
 
     Parameters
@@ -18,27 +18,27 @@ def _get_rot_matrix(m, pos, vel, r_corte=None):
     vel : `np.ndarray`, shape(n,3)
         Velocities of particles.
     r_corte : `float`, optional
-        The default is ``None``; if provided, it must be
-        positive and the rotation matrix `A` is calculated
-        for the particles with radii smaller than
-        r_corte.
-
+        The default is ``None``; if provided, it must be 
+        positive and the rotation matrix `A` is calculated 
+        for the particles with radii smaller than 
+        r_corte.                
+   
     Returns
     -------
     A : `np.ndarray`, shape(3,3)
         Rotation matrix.
     """
-
+    
     jx = m*(pos[:, 1]*vel[:, 2] - pos[:, 2]*vel[:, 1])
     jy = m*(pos[:, 2]*vel[:, 0] - pos[:, 0]*vel[:, 2])
     jz = m*(pos[:, 0]*vel[:, 1] - pos[:, 1]*vel[:, 0])
 
     r = np.sqrt(pos[:, 0]**2 + pos[:, 1]**2 + pos[:, 2]**2)
-
+    
     if r_corte is not None:
         mask = np.where(r < r_corte)
     else:
-        mask = np.repeat(True, len(r)),
+        mask = np.repeat(True, len(r)), 
 
     rjx = np.sum(jx[mask])
     rjy = np.sum(jy[mask])
@@ -61,25 +61,24 @@ def _get_rot_matrix(m, pos, vel, r_corte=None):
 
     A = np.asarray(
         (
-            [e1x, e1y, e1z],
-            [e2x, e2y, e2z],
-            [e3x, e3y, e3z]
+            [e1x,e1y,e1z],
+            [e2x,e2y,e2z],
+            [e3x,e3y,e3z]
             )
     )
-
+    
     return(A)
 
 
 def aling(m, pos, vel, r_corte):
-
-    """This rotates the positions, speeds and angular
-    moments of the particles so that the total angular
-    momentum match with the z-axis.
-
-    Optionally, only particles within a cutting radius
-    `(r_corte)` can be used to calculate the rotation
+    """This rotates the positions, speeds and angular 
+    moments of the particles so that the total angular 
+    momentum match with the z-axis. 
+    
+    Optionally, only particles within a cutting radius 
+    `(r_corte)` can be used to calculate the rotation 
     matrix.
-
+    
     Parameters
     ----------
     m : `np.ndarray`, shape(n,1)
@@ -89,10 +88,10 @@ def aling(m, pos, vel, r_corte):
     vel : `np.ndarray`, shape(n,3)
         Velocities of particles.
     r_corte : `float`, optional
-        The default is ``None``; if provided, it must be
-        positive and the rotation matrix `A` is calculated
+        The default is ``None``; if provided, it must be 
+        positive and the rotation matrix `A` is calculated 
         from the particles with radii smaller than r_corte.
-
+   
     Returns
     -------
     pos_rot : `np.ndarray`, shape(n,3)
@@ -100,14 +99,12 @@ def aling(m, pos, vel, r_corte):
     vel_rot : `np.ndarray`, shape(n,3)
         Rotated velocities of particles
     """
-
     A = _get_rot_matrix(m, pos, vel, r_corte)
 
     pos_rot = np.dot(A, pos.T)
     vel_rot = np.dot(A, vel.T)
-
+    
     return pos_rot.T, vel_rot.T
-
 
 ###################################################################
 
