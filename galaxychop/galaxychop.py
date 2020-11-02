f9d3e626aaec664cd02da896ef591156b9a41add
nvillagra <ntvillagra@gmail.com>
2020-11-02 14:27:36 -0300

    Trying to solve issue#57

--- galaxychop/galaxychop.py
+++ galaxychop/galaxychop.py
@@ -92,7 +92,6 @@ class Galaxy:
     components_g = attr.ib(default=None)
     metadata = attr.ib(default=None)
 
-
     def energy(self, pot_star, pot_dark, pot_gas):
         '''Calculation of kinetic and potencial energy of
         dark matter, star and gas particles'''
@@ -107,3 +106,84 @@ class Galaxy:
 
         return E_tot_star, E_tot_dark, E_tot_gas
 
+    def angular_momentum(self):
+        """Components of the angular momentum of stars, gas and DM.
+
+        Parameters
+        ----------
+
+        Coordinates and velocities for stars: x_s, y_s, z_s and
+         vx_s, vy_s, vz_s.
+
+        Coordinates and velocities for dark matter: x_dm, y_dm, z_dm and
+         vx_dm, vy_dm, vz_dm.
+
+        Coordinates and velocities for gas: x_g, y_g, z_g and vx_g, vy_g, vz_g.
+
+        masses of the stars: m_s.
+
+        Returns
+        -------
+
+        J_part = np.array(3, 3*n), concatenates angular momentum for the stars,
+        dark matter and gas in a single array.
+
+        Jr_star = np.array(n, 1), Component in the plane of the angular
+        momentum of stars.
+
+        Jr = np.array(3*n, 1) Component in the plane of the angular
+        momentum for all particles.
+
+        """
+
+        self.x_s = self.x_s - self.x_dm[p_dm.index(max(p_dm))]
+        self.y_s = self.y_s - self.y_dm[p_dm.index(max(p_dm))]
+        self.z_s = self.z_s - self.z_dm[p_dm.index(max(p_dm))]
+
+        self.x_dm = self.x_dm - self.x_dm[p_dm.index(max(p_dm))]
+        self.y_dm = self.y_dm - self.y_dm[p_dm.index(max(p_dm))]
+        self.z_dm = self.z_dm - self.z_dm[p_dm.index(max(p_dm))]
+
+        self.x_g = self.x_g - self.x_dm[p_dm.index(max(p_dm))]
+        self.y_g = self.y_g - self.y_dm[p_dm.index(max(p_dm))]
+        self.z_g = self.z_g - self.z_dm[p_dm.index(max(p_dm))]
+
+        pos_star_rot, vel_star_rot, A = aling(self.m_s,
+                                              [self.x_s, self.y_s, self.z_s],
+                                              [self.vx, self.vy, self.vz],
+                                              3 .R[j])
+
+        pos_dark_rot = rot([self.x_dm, self.y_dm, self.z_dm], A)
+        vel_dark_rot = rot([self.vx_dm, self.vy_dm, self.vz], A)
+
+        pos_gas_rot = rot([self.x_g, self.y_g, self.z_g], A)
+        vel_gas_rot = rot([self.vx_g, self.vy_g, self.vz_g], A)
+
+        J_dark = np.asarray((pos_dark_rot[:, 1] * vel_dark_rot[:, 2] -
+                             pos_dark_rot[:, 2] * vel_dark_rot[:, 1],
+                             pos_dark_rot[:, 2] * vel_dark_rot[:, 0] -
+                             pos_dark_rot[:, 0] * vel_dark_rot[:, 2],
+                             pos_dark_rot[:, 0] * vel_dark_rot[:, 1] -
+                             pos_dark_rot[:, 1] * vel_dark_rot[:, 0]))
+
+        J_star = np.asarray((pos_star_rot[:, 1] * vel_star_rot[:, 2] -
+                             pos_star_rot[:, 2] * vel_star_rot[:, 1],
+                             pos_star_rot[:, 2] * vel_star_rot[:, 0] -
+                             pos_star_rot[:, 0] * vel_star_rot[:, 2],
+                             pos_star_rot[:, 0] * vel_star_rot[:, 1] -
+                             pos_star_rot[:, 1] * vel_star_rot[:, 0]))
+
+        J_gas = np.asarray((pos_gas_rot[:, 1] * vel_gas_rot[:, 2] -
+                            pos_gas_rot[:, 2] * vel_gas_rot[:, 1],
+                            pos_gas_rot[:, 2] * vel_gas_rot[:, 0] -
+                            pos_gas_rot[:, 0] * vel_gas_rot[:, 2],
+                            pos_gas_rot[:, 0] * vel_gas_rot[:, 1] -
+                            pos_gas_rot[:, 1] * vel_gas_rot[:, 0]))
+
+        J_part = np.concatenate((J_gas, J_dark, J_star), axis=1)
+
+        Jr_star = np.sqrt(J_star[0, :]**2 + J_star[1, :]**2)
+
+        Jr = np.sqrt(J_part[0, :]**2 + J_part[1, :]**2)
+
+        return J_part, Jr_star, Jr
