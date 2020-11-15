6344bb17915041c2cea80da5bf21292d12081b39
Ornela Marioni <ornela@orne.iate.oac.uncor.edu>
2020-11-15 19:41:20 -0300

    change in units validation

--- galaxychop/galaxychop.py
+++ galaxychop/galaxychop.py
@@ -23,13 +23,58 @@ import dask.array as da
 # CONSTANTS
 # #####################################################
 
-G = 4.299e-6 * u.kpc * (u.km / u.s) ** 2 / u.M_sun
-G = G.to_value()
+"""Gravitational constant G.
+
+Units: kpc M_sun^-1 (km/s)^2
+"""
+G = 4.299e-6
+
+# #####################################################
+# UNIT CONVERTION
+# #####################################################
+
+
+@attr.s()
+class UnitConverter:
+    """
+    Unit converter.
+
+    This class returns a function that assigns the default units
+    to any dimensionless input or if the input is a u.Quantity
+    returns the units given.
+    """
+
+    default_unit = attr.ib()
+
+    def __call__(self, v):
+        """Caller function."""
+        if isinstance(v, u.Quantity) and v.unit != u.dimensionless_unscaled:
+            return v
+        return v * self.default_unit
+
+
+def unit_attribute(valid_units, default_unit, **kwargs):
+    """Define unit attribute of Galaxy class."""
+    if "converter" in kwargs:
+        raise AttributeError("converter")
+    converter = UnitConverter(default_unit=default_unit)
+
+    def _unit_validator(instance, attribute, value):
+        """Validate units."""
+        if value.unit.physical_type not in valid_units:
+            raise ValueError(f"Attribute '{attribute.name}'\
+                              must have units of {valid_units}.\
+                              Found {value.unit}")
+
+    validators = kwargs.pop("validator", [])
+    validators.append(_unit_validator)
+
+    return attr.ib(validator=validators, converter=converter, **kwargs)
+
 
 # #####################################################
 # GALAXY CLASS
 # #####################################################
-
 
 @attr.s(frozen=False)
 class Galaxy:
@@ -84,95 +129,95 @@ class Galaxy:
     ---------
     """
 
-    x_s = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    y_s = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    z_s = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    vx_s = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    vy_s = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    vz_s = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    m_s = attr.ib(validator=attr.validators.instance_of(u.Quantity))
+    x_s = unit_attribute(valid_units=['length'], default_unit=(u.kpc))
+    y_s = unit_attribute(valid_units=['length'], default_unit=(u.kpc))
+    z_s = unit_attribute(valid_units=['length'], default_unit=(u.kpc))
+    vx_s = unit_attribute(valid_units=['velocity'], default_unit=(u.km / u.s))
+    vy_s = unit_attribute(valid_units=['velocity'], default_unit=(u.km / u.s))
+    vz_s = unit_attribute(valid_units=['velocity'], default_unit=(u.km / u.s))
+    m_s = unit_attribute(valid_units=['mass'], default_unit=(u.Msun))
 
-    x_dm = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    y_dm = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    z_dm = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    vx_dm = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    vy_dm = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    vz_dm = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    m_dm = attr.ib(validator=attr.validators.instance_of(u.Quantity))
+    x_dm = unit_attribute(valid_units=['length'], default_unit=(u.kpc))
+    y_dm = unit_attribute(valid_units=['length'], default_unit=(u.kpc))
+    z_dm = unit_attribute(valid_units=['length'], default_unit=(u.kpc))
+    vx_dm = unit_attribute(valid_units=['velocity'], default_unit=(u.km / u.s))
+    vy_dm = unit_attribute(valid_units=['velocity'], default_unit=(u.km / u.s))
+    vz_dm = unit_attribute(valid_units=['velocity'], default_unit=(u.km / u.s))
+    m_dm = unit_attribute(valid_units=['mass'], default_unit=(u.Msun))
 
-    x_g = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    y_g = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    z_g = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    vx_g = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    vy_g = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    vz_g = attr.ib(validator=attr.validators.instance_of(u.Quantity))
-    m_g = attr.ib(validator=attr.validators.instance_of(u.Quantity))
+    x_g = unit_attribute(valid_units=['length'], default_unit=(u.kpc))
+    y_g = unit_attribute(valid_units=['length'], default_unit=(u.kpc))
+    z_g = unit_attribute(valid_units=['length'], default_unit=(u.kpc))
+    vx_g = unit_attribute(valid_units=['velocity'], default_unit=(u.km / u.s))
+    vy_g = unit_attribute(valid_units=['velocity'], default_unit=(u.km / u.s))
+    vz_g = unit_attribute(valid_units=['velocity'], default_unit=(u.km / u.s))
+    m_g = unit_attribute(valid_units=['mass'], default_unit=(u.Msun))
 
-    eps_s = attr.ib(default=0. * u.kpc,
-                    validator=attr.validators.instance_of(u.Quantity))
-    eps_dm = attr.ib(default=0. * u.kpc,
-                     validator=attr.validators.instance_of(u.Quantity))
-    eps_g = attr.ib(default=0. * u.kpc,
-                    validator=attr.validators.instance_of(u.Quantity))
+    eps_s = unit_attribute(default=0,
+                           valid_units=['length'], default_unit=(u.kpc))
+    eps_dm = unit_attribute(default=0.,
+                            valid_units=['length'], default_unit=(u.kpc))
+    eps_g = unit_attribute(default=0.,
+                           valid_units=['length'], default_unit=(u.kpc))
 
-    Etot_dm = attr.ib(default=None)
-    Etot_s = attr.ib(default=None)
-    Etot_g = attr.ib(default=None)
+    Etot_dm = unit_attribute(default=None,
+                             valid_units=['energy'],
+                             default_unit=(u.Msun * (u.km / u.s)**2))
+    Etot_s = unit_attribute(default=None,
+                            valid_units=['energy'],
+                            default_unit=(u.Msun * (u.km / u.s)**2))
+    Etot_g = unit_attribute(default=None,
+                            valid_units=['energy'],
+                            default_unit=(u.Msun * (u.km / u.s)**2))
 
-    components_s = attr.ib(default=None)
-    components_g = attr.ib(default=None)
-    metadata = attr.ib(default=None)
+    # components_s = attr.ib(default=None)
+    # components_g = attr.ib(default=None)
+    # metadata = attr.ib(default=None)
 
-    def __change_units_to_array__(f):
-        """
-        Decorate methods.
-
-        Change units and transform astropy Quantity to numpy array.
-        """
-        def new_method(self, *args, **kwargs):
-            self.x_s = self.x_s.to_value(u.kpc)
-            self.y_s = self.y_s.to_value(u.kpc)
-            self.z_s = self.z_s.to_value(u.kpc)
-            self.vx_s = self.vx_s.to_value(u.km / u.s)
-            self.vy_s = self.vy_s.to_value(u.km / u.s)
-            self.vz_s = self.vz_s.to_value(u.km / u.s)
-            self.m_s = self.m_s.to_value(u.M_sun)
-            self.eps_s = self.eps_s.to_value(u.kpc)
-
-            self.x_dm = self.x_dm.to_value(u.kpc)
-            self.y_dm = self.y_dm.to_value(u.kpc)
-            self.z_dm = self.z_dm.to_value(u.kpc)
-            self.vx_dm = self.vx_dm.to_value(u.km / u.s)
-            self.vy_dm = self.vy_dm.to_value(u.km / u.s)
-            self.vz_dm = self.vz_dm.to_value(u.km / u.s)
-            self.m_dm = self.m_dm.to_value(u.M_sun)
-            self.eps_dm = self.eps_dm.to_value(u.kpc)
-
-            self.x_g = self.x_g.to_value(u.kpc)
-            self.y_g = self.y_g.to_value(u.kpc)
-            self.z_g = self.z_g.to_value(u.kpc)
-            self.vx_g = self.vx_g.to_value(u.km / u.s)
-            self.vy_g = self.vy_g.to_value(u.km / u.s)
-            self.vz_g = self.vz_g.to_value(u.km / u.s)
-            self.m_g = self.m_g.to_value(u.M_sun)
-            self.eps_g = self.eps_g.to_value(u.kpc)
-
-            return f(self, *args, **kwargs)
-        return new_method
-
-    @__change_units_to_array__
     def energy(self):
         """
         Energy calculation.
 
-        Calculate kinetic and potencial energy of dark matter,
+        Calculate kinetic and potential energy of dark matter,
         star and gas particles.
         """
-        x = np.hstack((self.x_s, self.x_dm, self.x_g))
-        y = np.hstack((self.y_s, self.y_dm, self.y_g))
-        z = np.hstack((self.z_s, self.z_dm, self.z_g))
-        m = np.hstack((self.m_s, self.m_dm, self.m_g))
-        eps = np.max(self.eps_dm, self.eps_s, self.eps_s)
+        x_s = self.x_s.to_value(u.kpc)
+        y_s = self.y_s.to_value(u.kpc)
+        z_s = self.z_s.to_value(u.kpc)
+
+        x_g = self.x_g.to_value(u.kpc)
+        y_g = self.y_g.to_value(u.kpc)
+        z_g = self.z_g.to_value(u.kpc)
+
+        x_dm = self.x_dm.to_value(u.kpc)
+        y_dm = self.y_dm.to_value(u.kpc)
+        z_dm = self.z_dm.to_value(u.kpc)
+
+        m_s = self.m_s.to_value(u.Msun)
+        m_g = self.m_g.to_value(u.Msun)
+        m_dm = self.m_dm.to_value(u.Msun)
+
+        eps_s = self.eps_s.to_value(u.kpc)
+        eps_g = self.eps_g.to_value(u.kpc)
+        eps_dm = self.eps_dm.to_value(u.kpc)
+
+        vx_s = self.x_s.to_value(u.km / u.s)
+        vy_s = self.y_s.to_value(u.km / u.s)
+        vz_s = self.z_s.to_value(u.km / u.s)
+
+        vx_g = self.x_g.to_value(u.km / u.s)
+        vy_g = self.y_g.to_value(u.km / u.s)
+        vz_g = self.z_g.to_value(u.km / u.s)
+
+        vx_dm = self.x_dm.to_value(u.km / u.s)
+        vy_dm = self.y_dm.to_value(u.km / u.s)
+        vz_dm = self.z_dm.to_value(u.km / u.s)
+
+        x = np.hstack((x_s, x_dm, x_g))
+        y = np.hstack((y_s, y_dm, y_g))
+        z = np.hstack((z_s, z_dm, z_g))
+        m = np.hstack((m_s, m_dm, m_g))
+        eps = np.max([eps_dm, eps_g, eps_s])
 
         pot = utils.potential(da.asarray(x, chunks=100),
                               da.asarray(y, chunks=100),
@@ -180,20 +225,22 @@ class Galaxy:
                               da.asarray(m, chunks=100),
                               da.asarray(eps))
 
-        pot_star = pot[:len(self.m_s)]
-        pot_dark = pot[len(self.m_s):len(self.m_s) + len(self.m_dm)]
-        pot_gas = pot[len(self.m_s) + len(self.m_dm):]
+        pot_s = pot[:len(m_s)]
+        pot_dm = pot[len(m_s):len(m_s) + len(m_dm)]
+        pot_g = pot[len(m_s) + len(m_dm):]
 
-        k_dm = 0.5 * (self.vx_dm**2 + self.vy_dm**2 + self.vz_dm**2)
-        k_s = 0.5 * (self.vx_s**2 + self.vy_s**2 + self.vz_s**2)
-        k_g = 0.5 * (self.vx_g**2 + self.vy_g**2 + self.vz_g**2)
+        k_dm = 0.5 * (vx_dm**2 + vy_dm**2 + vz_dm**2)
+        k_s = 0.5 * (vx_s**2 + vy_s**2 + vz_s**2)
+        k_g = 0.5 * (vx_g**2 + vy_g**2 + vz_g**2)
 
-        E_tot_dark = k_dm - pot_dark
-        E_tot_star = k_s - pot_star
-        E_tot_gas = k_g - pot_gas
+        Etot_dm = k_dm - pot_dm
+        Etot_s = k_s - pot_s
+        Etot_g = k_g - pot_g
 
-        setattr(self, "E_tot_dark", E_tot_dark)
-        setattr(self, "E_tot_star", E_tot_star)
-        setattr(self, "E_tot_gas", E_tot_gas)
+        setattr(self, "Etot_dm", Etot_dm * u.Msun * (u.km / u.s)**2)
+        setattr(self, "Etot_s", Etot_s * u.Msun * (u.km / u.s)**2)
+        setattr(self, "Etot_g", Etot_g * u.Msun * (u.km / u.s)**2)
 
-        return E_tot_dark, E_tot_star, E_tot_gas
+        return Etot_dm * u.Msun * (u.km / u.s)**2,
+        Etot_s * u.Msun * (u.km / u.s)**2,
+        Etot_g * u.Msun * (u.km / u.s)**2
