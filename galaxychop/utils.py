392a95062496a838992584ec7a31c1288a4ec6e5
vcristiani <valeria.cristiani@unc.edu.ar>
2020-11-18 00:21:29 -0300

    modification of the aling function to correct the way it works

--- galaxychop/utils.py
+++ galaxychop/utils.py
@@ -72,7 +72,9 @@ def _get_rot_matrix(m, pos, vel, r_corte=None):
     return A
 
 
-def aling(m, pos, vel, r_corte):
+def aling(m_s, x_s, y_s, z_s, vx_s, vy_s, vz_s,
+          m_dm, x_dm, y_dm, z_dm, vx_dm, vy_dm, vz_dm,
+          m_g, x_g, y_g, z_g, vx_g, vy_g, vz_g, r_corte):
     """
     Aling the galaxy.
 
@@ -101,12 +103,34 @@ def aling(m, pos, vel, r_corte):
     vel_rot : `np.ndarray`, shape(n,3)
         Rotated velocities of particles
     """
-    A = _get_rot_matrix(m, pos, vel, r_corte)
+    pos = np.vstack((x_s, y_s, z_s)).T
+    vel = np.vstack((vx_s, vy_s, vz_s)).T
 
-    pos_rot = np.dot(A, pos.T)
-    vel_rot = np.dot(A, vel.T)
+    A = _get_rot_matrix(m_s, pos, vel, r_corte)
 
-    return pos_rot.T, vel_rot.T
+    pos_rot_s = np.dot(A, pos.T)
+    vel_rot_s = np.dot(A, vel.T)
+
+    pos = np.vstack((x_dm, y_dm, z_dm)).T
+    vel = np.vstack((vx_dm, vy_dm, vz_dm)).T
+
+    pos_rot_dm = np.dot(A, pos.T)
+    vel_rot_dm = np.dot(A, vel.T)
+
+    pos = np.vstack((x_g, y_g, z_g)).T
+    vel = np.vstack((vx_g, vy_g, vz_g)).T
+
+    pos_rot_g = np.dot(A, pos.T)
+    vel_rot_g = np.dot(A, vel.T)
+
+    return (
+        pos_rot_s.T[:, 0], pos_rot_s.T[:, 1], pos_rot_s.T[:, 2],
+        vel_rot_s.T[:, 0], vel_rot_s.T[:, 1], vel_rot_s.T[:, 2],
+        pos_rot_dm.T[:, 0], pos_rot_dm.T[:, 1], pos_rot_dm.T[:, 2],
+        vel_rot_dm.T[:, 0], vel_rot_dm.T[:, 1], vel_rot_dm.T[:, 2],
+        pos_rot_g.T[:, 0], pos_rot_g.T[:, 1], pos_rot_g.T[:, 2],
+        vel_rot_g.T[:, 0], vel_rot_g.T[:, 1], vel_rot_g.T[:, 2]
+    )
 
 
 @dask.delayed
@@ -127,10 +151,12 @@ def _potential_dask(x, y, z, m, eps):
     -------
     Specific potential energy of particles
     """
-    dist = np.sqrt(np.square(x - x.reshape(-1, 1))
-                   + np.square(y - y.reshape(-1, 1))
-                   + np.square(z - z.reshape(-1, 1))
-                   + np.square(eps))
+    dist = np.sqrt(
+        np.square(x - x.reshape(-1, 1))
+        + np.square(y - y.reshape(-1, 1))
+        + np.square(z - z.reshape(-1, 1))
+        + np.square(eps)
+    )
 
     np.fill_diagonal(dist, 0.0)
 
@@ -140,7 +166,7 @@ def _potential_dask(x, y, z, m, eps):
     return mdist.sum(axis=1) * G
 
 
-def potential(x, y, z, m, eps=0.):
+def potential(x, y, z, m, eps=0.0):
     """Compute de potential energy."""
     pot = _potential_dask(x, y, z, m, eps)
     return np.asarray(pot.compute())
