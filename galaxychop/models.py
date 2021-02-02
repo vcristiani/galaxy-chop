d91b86cdcdaaca132c0071f08aeca6423f4f6b19
vcristiani <71674820+vcristiani@users.noreply.github.com>
2021-02-02 18:53:39 -0300

    Update models.py
    styles changes

--- galaxychop/models.py
+++ galaxychop/models.py
@@ -394,7 +394,7 @@ class GCAutogmm(GCClusterMixin, TransformerMixin):
         number_of_gaussians = np.min(self.component_to_try[mask])
         self.n_components = number_of_gaussians
 
-        # Clustering with gaussian mixture and the parameters obtained. 
+        # Clustering with gaussian mixture and the parameters obtained.
         gcgmm_ = GaussianMixture(n_components=number_of_gaussians)
         self.gcgmm_ = gcgmm_
         labels = gcgmm_.fit(X).predict(X)
