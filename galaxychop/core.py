24b6747e7e050e8e3cff5cc8e94a01b16916beae
RafaelPignata622 <rafael.pignata@unc.edu.ar>
2021-02-03 15:05:51 -0300

    Modifiqué el core y el tutorial para que funcionaran bien

--- galaxychop/core.py
+++ galaxychop/core.py
@@ -336,13 +336,13 @@ class Galaxy:
 
             X_s = np.hstack(
                 (
-                    self.arr_.m_s.reshape(n_s, 1),
                     self.arr_.x_s.reshape(n_s, 1),
                     self.arr_.y_s.reshape(n_s, 1),
                     self.arr_.z_s.reshape(n_s, 1),
                     self.arr_.vx_s.reshape(n_s, 1),
                     self.arr_.vy_s.reshape(n_s, 1),
                     self.arr_.vz_s.reshape(n_s, 1),
+                    self.arr_.m_s.reshape(n_s, 1),
                     self.paramcirc[0].reshape(n_s, 1),
                     self.paramcirc[1].reshape(n_s, 1),
                     self.paramcirc[2].reshape(n_s, 1),
@@ -419,40 +419,63 @@ class Galaxy:
         y_g = self.arr_.y_g
         z_g = self.arr_.z_g
 
+        pot_s = self.arr_.pot_s
+        pot_dm = self.arr_.pot_dm
+        pot_g = self.arr_.pot_g
+
         eps_s = self.arr_.eps_s
         eps_dm = self.arr_.eps_dm
         eps_g = self.arr_.eps_g
 
-        x = np.hstack((x_s, x_dm, x_g))
-        y = np.hstack((y_s, y_dm, y_g))
-        z = np.hstack((z_s, z_dm, z_g))
-        m = np.hstack((m_s, m_dm, m_g))
-        eps = np.max([eps_s, eps_dm, eps_g])
-
-        pot = utils.potential(
-            da.asarray(x, chunks=100),
-            da.asarray(y, chunks=100),
-            da.asarray(z, chunks=100),
-            da.asarray(m, chunks=100),
-            da.asarray(eps),
+        potential = np.concatenate(
+            [
+                pot_s,
+                pot_dm,
+                pot_g,
+            ]
         )
 
-        num_s = len(m_s)
-        num = len(m_s) + len(m_dm)
+        if np.all(potential == 0.0):
+            x = np.hstack((x_s, x_dm, x_g))
+            y = np.hstack((y_s, y_dm, y_g))
+            z = np.hstack((z_s, z_dm, z_g))
+            m = np.hstack((m_s, m_dm, m_g))
+            eps = np.max([eps_s, eps_dm, eps_g])
 
-        pot_s = pot[:num_s]
-        pot_dm = pot[num_s:num]
-        pot_g = pot[num:]
+            pot = utils.potential(
+                da.asarray(x, chunks=100),
+                da.asarray(y, chunks=100),
+                da.asarray(z, chunks=100),
+                da.asarray(m, chunks=100),
+                da.asarray(eps),
+            )
 
-        new = attr.asdict(self, recurse=False)
-        del new["arr_"]
-        new.update(
-            pot_s=-pot_s * (u.km / u.s) ** 2,
-            pot_dm=-pot_dm * (u.km / u.s) ** 2,
-            pot_g=-pot_g * (u.km / u.s) ** 2,
-        )
+            num_s = len(m_s)
+            num = len(m_s) + len(m_dm)
 
-        return Galaxy(**new)
+            pot_s = pot[:num_s]
+            pot_dm = pot[num_s:num]
+            pot_g = pot[num:]
+
+            new = attr.asdict(self, recurse=False)
+            del new["arr_"]
+            new.update(
+                pot_s=-pot_s * (u.km / u.s) ** 2,
+                pot_dm=-pot_dm * (u.km / u.s) ** 2,
+                pot_g=-pot_g * (u.km / u.s) ** 2,
+            )
+
+            return Galaxy(**new)
+        else:
+            new = attr.asdict(self, recurse=False)
+            del new["arr_"]
+            new.update(
+                pot_s=-pot_s * (u.km / u.s) ** 2,
+                pot_dm=-pot_dm * (u.km / u.s) ** 2,
+                pot_g=-pot_g * (u.km / u.s) ** 2,
+            )
+
+            return Galaxy(**new)
 
     @property
     def energy(self):
