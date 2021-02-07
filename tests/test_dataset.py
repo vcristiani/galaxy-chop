0687d1023f09102d2eb53d3e5b04fdeff61c7dd6
vcristiani <valeria.cristiani@unc.edu.ar>
2021-02-07 13:33:39 -0300

    dataset test correction

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -38,41 +38,41 @@ def test_load_star():
 
 def test_load_dark_TNG_394242():
     """Check the dark_ID_394242.npy array len."""
-    result = gc.dataset.load_gas_394242()
+    result = gc.dataset.load_dark_394242()
 
-    assert result.shape == (155101, 8)
+    assert result.shape == (21156, 8)
 
 
 def test_load_gas_TNG_394242():
     """Check the gas_ID_394242.npy array len."""
-    result = gc.dataset.load_dark_394242()
+    result = gc.dataset.load_gas_394242()
 
-    assert result.shape == (80153, 8)
+    assert result.shape == (4061, 8)
 
 
 def test_load_star_TNG_394242():
     """Check the star_ID_394242.npy array len."""
     result = gc.dataset.load_star_394242()
 
-    assert result.shape == (37393, 8)
+    assert result.shape == (32067, 8)
 
 
 def test_load_pot_dark_TNG_394242():
     """Check the potential_dark_ID_394242.npy array len."""
     result = gc.dataset.load_pot_dark_394242()
 
-    assert result.shape == (155101,)
+    assert result.shape == (21156,)
 
 
 def test_load_pot_gas_TNG_394242():
     """Check the potential_gas_ID_394242.npy array len."""
     result = gc.dataset.load_pot_gas_394242()
 
-    assert result.shape == (80153,)
+    assert result.shape == (4061,)
 
 
 def test_load_pot_star_TNG_394242():
     """Check the potential_star_ID_394242.npy array len."""
     result = gc.dataset.load_pot_star_394242()
 
-    assert result.shape == (37393,)
+    assert result.shape == (32067,)
