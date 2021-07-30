d2f5dd1558ed08e521e3f5683e2f9b8142920a67
vcristiani <valeria.cristiani@unc.edu.ar>
2021-07-30 10:06:30 -0300

    I added the functions that read hdf5 and .npy. These also use mkgalaxy to instantiate the galaxy directly without having to load one by one: masses, positions, velocities, etc.

--- galaxychop/core.py
+++ galaxychop/core.py
@@ -14,6 +14,7 @@ import enum
 from collections import defaultdict
 
 from astropy import units as u
+from astropy.table import Table
 
 import attr
 
@@ -24,6 +25,8 @@ import numpy as np
 import pandas as pd
 
 import uttr
+
+import h5py
 
 
 # =============================================================================
@@ -890,4 +893,106 @@ def mkgalaxy(
         potential=pot_g,
     )
     galaxy = Galaxy(stars=stars, dark_matter=dark_matter, gas=gas)
+    return galaxy
+
+
+def _table_to_dict(table, key_suffix):
+    kws = {f"{k}_{key_suffix}": v for k, v in table.items() if k != "id"}
+    kws[f"pot_{key_suffix}"] = kws.pop(f"potential_{key_suffix}", None)
+
+    return kws
+
+
+def read_hdf5(
+    path,
+    softening_s: float = 0.0,
+    softening_dm: float = 0.0,
+    softening_g: float = 0.0,
+):
+
+    with h5py.File(path, "r") as f:
+        star_table = Table.read(f["stars"])
+        dark_table = Table.read(f["dark_matter"])
+        gas_table = Table.read(f["gas"])
+
+    galaxy_kws = {
+        "softening_s": softening_s,
+        "softening_dm": softening_dm,
+        "softening_g": softening_g,
+    }
+
+    star_kws = _table_to_dict(star_table, "s")
+    galaxy_kws.update(star_kws)
+
+    dark_kws = _table_to_dict(dark_table, "dm")
+    galaxy_kws.update(dark_kws)
+
+    gas_kws = _table_to_dict(gas_table, "g")
+    galaxy_kws.update(gas_kws)
+
+    galaxy = mkgalaxy(**galaxy_kws)
+
+    return galaxy
+
+
+def read_file(
+    path_star,
+    path_dark,
+    path_gas,
+    columns,
+    path_pot_s=None,
+    path_pot_dm=None,
+    path_pot_g=None,
+    softening_s: float = 0.0,
+    softening_dm: float = 0.0,
+    softening_g: float = 0.0,
+):
+
+    # Leo los files de las particulas
+    particles_star = np.load(path_star)
+    particles_dark = np.load(path_dark)
+    particles_gas = np.load(path_gas)
+
+    # Armo los dataframes
+    df_star = pd.DataFrame(particles_star, columns=columns)
+    df_dark = pd.DataFrame(particles_dark, columns=columns)
+    df_gas = pd.DataFrame(particles_gas, columns=columns)
+
+    # Leo los files de los potenciales si es que existen
+    # y agrego la columna a los dataframes
+    if path_pot_s is not None:
+        pot_s = np.load(path_pot_s)
+        df_star.insert(7, "potential", pot_s, True)
+
+    if path_pot_dm is not None:
+        pot_dm = np.load(path_pot_dm)
+        df_dark.insert(7, "potential", pot_dm, True)
+
+    if path_pot_g is not None:
+        pot_g = np.load(path_pot_g)
+        df_gas.insert(7, "potential", pot_g, True)
+
+    # Convierto los dataframes en tablas de astropy
+    star_table = Table.from_pandas(df_star)
+    dark_table = Table.from_pandas(df_dark)
+    gas_table = Table.from_pandas(df_gas)
+
+    # De aca para abajo lo mismo que en el otro
+    galaxy_kws = {
+        "softening_s": softening_s,
+        "softening_dm": softening_dm,
+        "softening_g": softening_g,
+    }
+
+    star_kws = _table_to_dict(star_table, "s")
+    galaxy_kws.update(star_kws)
+
+    dark_kws = _table_to_dict(dark_table, "dm")
+    galaxy_kws.update(dark_kws)
+
+    gas_kws = _table_to_dict(gas_table, "g")
+    galaxy_kws.update(gas_kws)
+
+    galaxy = mkgalaxy(**galaxy_kws)
+
     return galaxy
