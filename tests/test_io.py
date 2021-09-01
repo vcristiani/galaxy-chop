b83de437a2ae482309e4a761fd9f9e01c3e96fe5
vcristiani <valeria.cristiani@unc.edu.ar>
2021-08-31 22:04:43 -0300

    I corrected the test paths of test_io.py. Added the test_data folder with the files needed to do the tests in test_io.py.

--- tests/test_io.py
+++ tests/test_io.py
@@ -20,22 +20,26 @@ from pathlib import Path
 # =============================================================================
 # PATHS
 # =============================================================================
-PATH = Path(os.path.abspath(os.path.dirname(".")))
+PATH = Path(os.path.abspath(os.path.dirname(__file__)))
+TEST_DATA_PATH = PATH / "test_data"
+
 
 # =============================================================================
 # IO TESTS
 # =============================================================================
-path_star = str(PATH) + "/galaxychop/dataset/star_ID_394242.npy"
-path_gas = str(PATH) + "/galaxychop/dataset/gas_ID_394242.npy"
-path_dark = str(PATH) + "/galaxychop/dataset/dark_ID_394242.npy"
 
-columns=['m', 'x', 'y', 'z', 'vx', 'vy', 'vz','id']
-
-path_pot_s = str(PATH) + "/galaxychop/dataset/potential_star_ID_394242.npy"
-path_pot_dm = str(PATH) + "/galaxychop/dataset/potential_dark_ID_394242.npy"
-path_pot_g = str(PATH) + "/galaxychop/dataset/potential_gas_ID_394242.npy"
 
 def test_read_file_method():
+    path_star = TEST_DATA_PATH / "star_ID_394242.npy"
+    path_gas = TEST_DATA_PATH / "gas_ID_394242.npy"
+    path_dark = TEST_DATA_PATH / "dark_ID_394242.npy"
+
+    columns = ["m", "x", "y", "z", "vx", "vy", "vz", "id"]
+
+    path_pot_s = TEST_DATA_PATH / "potential_star_ID_394242.npy"
+    path_pot_dm = TEST_DATA_PATH / "potential_dark_ID_394242.npy"
+    path_pot_g = TEST_DATA_PATH / "potential_gas_ID_394242.npy"
+
     gala = io.read_file(
         path_star=path_star,
         path_dark=path_dark,
@@ -43,13 +47,14 @@ def test_read_file_method():
         columns=columns,
         path_pot_s=path_pot_s,
         path_pot_dm=path_pot_dm,
-        path_pot_g=path_pot_g
+        path_pot_g=path_pot_g,
     )
 
-    assert isinstance(gala, core.Galaxy) == True
-    
+    assert isinstance(gala, core.Galaxy) is True
+
+
 def test_read_hdf5_method():
-    path = str(PATH) + "/galaxychop/dataset/gal394242.h5"
+    path = TEST_DATA_PATH / "gal394242.h5"
     gala = io.read_hdf5(path=path)
 
-    assert isinstance(gala, core.Galaxy) == True
+    assert isinstance(gala, core.Galaxy) is True
