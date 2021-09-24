4fa241e1f1d118dfddb076d14b4ce28cfd6ca9c4
JuanBC <jbc.develop@gmail.com>
2021-09-24 19:10:32 -0300

    license

--- galaxychop/plot.py
+++ galaxychop/plot.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# License: BSD-3 (https://tldrlegal.com/license/bsd-3-clause-license-(revised))
-# Copyright (c) 2016-2021, Cabral, Juan; Luczywo, Nadia
-# All rights reserved.
+# This file is part of
+# the galaxy-chop project (https://github.com/vcristiani/galaxy-chop)
+# Copyright (c) 2020, Valeria Cristiani
+# License: MIT
+# Full Text: https://github.com/vcristiani/galaxy-chop/blob/master/LICENSE.txt
 
 # =============================================================================
 # DOCS
