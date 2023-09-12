226b027bfcb39e20ac8194f0ba39f4cb51ac8b0d
juanbc <jbcabral@unc.edu.ar>
2023-09-12 00:00:41 -0300

    new header

--- .header-template
+++ .header-template
@@ -1,5 +1,5 @@
 # This file is part of
 # the galaxy-chop project (https://github.com/vcristiani/galaxy-chop)
-# Copyright (c) 2021, Valeria Cristiani
+# Copyright (c) Cristiani, et al. 2021, 2022, 2023
 # License: MIT
 # Full Text: https://github.com/vcristiani/galaxy-chop/blob/master/LICENSE.txt
--- galaxychop/models/histogram.py
+++ galaxychop/models/histogram.py
@@ -1,6 +1,6 @@
 # This file is part of
 # the galaxy-chop project (https://github.com/vcristiani/galaxy-chop)
-# Copyright (c) 2021, Valeria Cristiani
+# Copyright (c) Cristiani, et al. 2021, 2022, 2023
 # License: MIT
 # Full Text: https://github.com/vcristiani/galaxy-chop/blob/master/LICENSE.txt
 
