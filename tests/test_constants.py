935394370f05662243b6aad24ff8522ebd6501a4
juanbc <jbcabral@unc.edu.ar>
2023-09-15 11:39:02 -0300

    bump styles and qa

---
+++ tests/test_constants.py
@@ -0,0 +1,11 @@
+# This file is part of
+# the galaxy-chop project (https://github.com/vcristiani/galaxy-chop)
+# Copyright (c) Cristiani, et al. 2021, 2022, 2023
+# License: MIT
+# Full Text: https://github.com/vcristiani/galaxy-chop/blob/master/LICENSE.txt
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+# nothing to test here!
